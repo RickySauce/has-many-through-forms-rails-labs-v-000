d1b212098e41c86edf06d75159c60f1052d857c0
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-23 10:57:53 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   accepts_nested_attributes_for :user
-  validates :content, presence: true 
-  validates :user_id, presence: true 
+  validates :content, presence: true
+  validates :user_id, presence: true
   validates :post_id, presence: true
 
 end
