2060d4ff87b4de246507052656e3f10475521610
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-23 10:00:06 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -20,11 +20,11 @@ ActiveRecord::Schema.define(version: 20160119152016) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string   "comment_content"
+    t.string   "content"
     t.integer  "user_id"
     t.integer  "post_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
